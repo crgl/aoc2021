a9f19ffce8c3ebc6b3c46146c069db1d2eb460fc
Charles Gleason <charles_gleason@alumni.brown.edu>
2021-12-05 14:26:23 -0500

    Add d2 part 1 solution

---
+++ d2/d2.jl
@@ -0,0 +1,38 @@
+function parse_line(s)
+    words = split(s)
+    (words[1], parse(Int64, words[2]))
+end
+
+function get_input()
+    map(parse_line, readlines("input/input.txt"))
+end
+
+function p1(vals)
+    hor = 0
+    vert = 0
+    for (dir, n) in vals
+        if dir == "forward"
+            hor += n
+        elseif dir == "down"
+            vert += n
+        elseif dir == "up"
+            vert -= n
+        else
+            println(dir, " is not supported")
+            exit(1)
+        end
+    end
+    println("P1: ", hor * vert)
+end
+
+function p2(vals)
+    println("P2: ", "???")
+end
+
+function solve()
+    vals = get_input()
+    p1(vals)
+    p2(vals)
+end
+
+solve()
