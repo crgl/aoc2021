ca202932a3feaebec77cf18828b0af3ec081fa58
Charles Gleason <charles_gleason@alumni.brown.edu>
2021-12-05 14:06:00 -0500

    Add d1 part 2 solution

--- d1/d1.jl
+++ d1/d1.jl
@@ -5,7 +5,7 @@ end
 function p1(vals)
     output = 0
     base = if length(vals) < 1
-    0
+        0
     else
         vals[1]
     end
@@ -18,9 +18,28 @@ function p1(vals)
     println("P1: ", output)
 end
 
+function p2(vals)
+    output = 0
+    base = if length(vals) < 4
+        0
+    else
+        sum(vals[1:3])
+    end
+    new_val = base
+    for tup in zip(vals, vals[2:end], vals[3:end])
+        new_val = sum(tup)
+        if new_val > base
+            output += 1
+        end
+        base = new_val
+    end
+    println("P2: ", output)
+end
+
 function solve()
     vals = get_input()
     p1(vals)
+    p2(vals)
 end
 
 solve()
