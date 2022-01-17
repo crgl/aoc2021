ea5e91272ea8a9db64ffe22a6a2bbe87121c03b4
Charles Gleason <charles_gleason@alumni.brown.edu>
2022-01-17 16:33:34 -0500

    Add d5 p1 and p2 solutions

---
+++ d5/d5.jl
@@ -0,0 +1,117 @@
+function get_input()
+    lines = readlines("input/input.txt")
+    n = length(lines)
+    vents = Array{Vector{Int64}}(undef, n)
+    for (i, line) in enumerate(lines)
+        segment = parse.(Int64, vcat(split.(split(line, " -> "), ",")...))
+        vents[i] = segment
+        # p1, p2 = split(line, " -> ")
+        # p1 = parse.(Int64, split(p1, ","))
+        # p2 = parse.(Int64, split(p2, ","))
+        # println(p1)
+        # println(p2)
+    end
+    vents
+end
+
+function p1(vents)
+    vent_counts = Dict()
+    for vent in vents
+        x1, y1, x2, y2 = vent
+        if y1 > y2
+            y1, y2 = y2, y1
+        end
+        if x1 > x2
+            x1, x2 = x2, x1
+        end
+        if x1 == x2
+            for y in range(y1, y2)
+                tup = (x1, y)
+                if tup in keys(vent_counts)
+                    vent_counts[tup] += 1
+                else
+                    vent_counts[tup] = 1
+                end
+            end
+        elseif y1 == y2
+            for x in range(x1, x2)
+                tup = (x, y1)
+                if tup in keys(vent_counts)
+                    vent_counts[tup] += 1
+                else
+                    vent_counts[tup] = 1
+                end
+            end
+        end
+    end
+    output = 0
+    for val in values(vent_counts)
+        if val > 1
+            output += 1
+        end
+    end
+    println("P1: ", output)
+end
+
+function p2(vents)
+    vent_counts = Dict()
+    for vent in vents
+        x1, y1, x2, y2 = vent
+        ystep = if y1 > y2
+            -1
+        else
+            1
+        end
+        xstep = if x1 > x2
+            -1
+        else
+            1
+        end
+        if x1 == x2
+            for y in range(y1, y2, step = ystep)
+                tup = (x1, y)
+                if tup in keys(vent_counts)
+                    vent_counts[tup] += 1
+                else
+                    vent_counts[tup] = 1
+                end
+            end
+        elseif y1 == y2
+            for x in range(x1, x2, step = xstep)
+                tup = (x, y1)
+                if tup in keys(vent_counts)
+                    vent_counts[tup] += 1
+                else
+                    vent_counts[tup] = 1
+                end
+            end
+        else
+            for tup in zip(range(x1, x2, step = xstep), range(y1, y2, step = ystep))
+                if tup in keys(vent_counts)
+                    vent_counts[tup] += 1
+                else
+                    vent_counts[tup] = 1
+                end
+            end
+        end
+    end
+    output = 0
+    for val in values(vent_counts)
+        if val > 1
+            output += 1
+        end
+    end
+    println("P2: ", output)
+end
+
+function solve()
+    vents = get_input()
+    if length(vents) == 0
+        println("Empty input!")
+        exit(1)
+    end
+    p1(vents)
+    p2(vents)
+end
+
+solve()
